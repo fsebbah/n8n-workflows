bf730d64895fa0e423273666347e24e587c7a60a
Franck Sebbah <franck.sebbah@gmail.com>
2026-01-04 18:03:59 +0100

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -47,7 +47,7 @@ module.exports = {
 
         // API Torah
         TORAH_API_URL: 'http://pi6.local:3031',
-        // TORAH_API_KEY: 'votre-clé-ici',  // À décommenter et remplir si authentification requise
+        TORAH_API_KEY: '17ae129e4b49828e7439cae4949803e0a78d3725ff5dd76857e32d071f33af26',  // À décommenter et remplir si authentification requise
       },
 
       // Environnement production (moins de logs)
